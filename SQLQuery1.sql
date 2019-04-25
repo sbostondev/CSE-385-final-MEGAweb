7d1110b6a631b38fb0821753cf7eac92fa17e00e
Steven Kyle <kylesm@miamioh.edu>
2019-04-25 13:08:54 -0400

    Adding project to git

---
+++ SQLQuery1.sql
@@ -0,0 +1,50 @@
+﻿	SELECT hardwareName
+	FROM HardwareDetail
+	WHERE hardwareDetailId IN (
+		SELECT itemDetailId
+		FROM Item
+		WHERE itemType = 'c'
+	)
+	FOR XML PATH('Console'), ROOT('Consoles')
+
+DECLARE @myid uniqueidentifier = NEWID()
+INSERT INTO Item (itemId, itemBarcode, itemDetailId, itemType, itemStatus, itemPurchaseDate)
+VALUES(
+	12,
+	260001342,
+	@myid,
+	'g',
+	'a',
+	SYSDATETIME()
+)
+
+
+INSERT INTO Genre(genreId, genreName)
+Values(4, 'beat em up')
+
+INSERT INTO GameDetail(gameDetailId, gameName, gamePublisher, gameGenreId, gameConsole, popularity)
+VALUES(
+	@myid,
+	'Battletoads',
+	'Xbox Game Studios',
+	4,
+	'41AFDEBF-588B-4C28-91A9-370A462C98D6',
+	0
+)
+
+select * from GameDetail
+
+SELECT * FROM HardwareDetail
+
+EXEC spGetByConsoleManufacturer --@manufacturer = 'Sony'
+GO
+
+CREATE PROCEDURE spGetAllManufacturers
+AS
+	SELECT DISTINCT hardwareManufacturer
+	FROM HardwareDetail
+	WHERE hardwareDetailId IN (
+		SELECT itemDetailId
+		FROM Item
+		WHERE itemType = 'c'
+	)
---
+++ SimpleWebsiteAndAPI/DB_Script.sql
@@ -0,0 +1,57 @@
+﻿--=========================================
+DROP PROCEDURE IF EXISTS spGetVendors
+CREATE PROCEDURE spGetVendors
+	@pageNum INT = 0,
+	@records INT = -1
+AS
+	SET NOCOUNT ON
+    
+	IF(@records = -1) BEGIN
+		SELECT	VendorName, VendorCity, 
+				VendorState, VendorZipCode
+		FROM dbo.Vendors
+		ORDER BY VendorState, VendorCity
+	END ELSE BEGIN
+		SELECT	VendorName, VendorCity, 
+				VendorState, VendorZipCode
+		FROM dbo.Vendors
+		ORDER BY VendorState, VendorCity
+			OFFSET (@pageNum * @records) ROWS
+			FETCH NEXT @records ROWS ONLY
+	END
+GO
+
+--=========================================
+DROP PROCEDURE IF EXISTS spUpdateInvoicePaymentTotal
+CREATE PROCEDURE spUpdateInvoicePaymentTotal
+	@invoiceId int,
+	@paymentTotal MONEY
+AS
+	UPDATE dbo.Invoices
+	SET PaymentTotal = @paymentTotal
+	WHERE InvoiceID = @invoiceId
+GO
+
+--=========================================
+DROP PROCEDURE IF EXISTS spGetVendorDetails
+CREATE PROCEDURE spGetVendorDetails
+	@VendorID INT
+AS
+	SET NOCOUNT ON
+    
+	SELECT	v.VendorContactFName + ' ' + v.VendorContactLName AS VendorContact,
+			[Invoices] = (
+				SELECT	i.InvoiceID,
+						i.InvoiceNumber,
+						i.InvoiceDate,
+						i.PaymentTotal,
+						i.Balance
+				FROM vwInvoices i
+				WHERE v.VendorID = i.VendorID
+				FOR JSON PATH
+			)
+	FROM dbo.Vendors v
+	WHERE v.VendorID = @VendorID
+	ORDER BY v.VendorName	
+	FOR JSON PATH
+GO
